314dac1b6e67e32bedfde1e0c75669e589f5453e
23navi <navisureka23@gmail.com>
2023-08-28 21:42:46 +0530

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [input,setInput] = useState("") // Code which user will type into the textbox
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <textarea></textarea>
+      <div>
+        <button>Submit</button>
+      </div>
+      <pre></pre>
     </div>
   );
 }
