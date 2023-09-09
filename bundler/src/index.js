88dc6658a44339bb834712035252f0eb10073e9d
23navi <navisureka23@gmail.com>
2023-09-09 18:25:54 +0530

    buldler example

---
+++ bundler/src/index.js
@@ -0,0 +1,4 @@
+const message = require("./message");
+console.log(message);
+
+/// To compile the this, make sure you have installed webpack to the exect version and run ->   export NODE_OPTIONS=--openssl-legacy-provider  : in terminal
