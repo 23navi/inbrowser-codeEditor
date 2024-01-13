d10d49cc0a6251ae800114dc5e15743ae52d4f96
23navi <navisureka23@gmail.com>
2024-01-13 09:18:57 +0530

    Breaking multiple onLoad

--- in-browser-transpiling/src/plugins/fetch-plugin.ts
+++ in-browser-transpiling/src/plugins/fetch-plugin.ts
@@ -10,14 +10,7 @@ export const fetchPlugin = (userCode: string) => {
   return {
     name: "fetch-plugin",
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === "index.js") {
-          return {
-            loader: "jsx",
-            contents: userCode,
-          };
-        }
-
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
         // Check if the data is in the cache
         const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
           args.path
@@ -30,25 +23,54 @@ export const fetchPlugin = (userCode: string) => {
         // Fetch the data from unpkg.com. We need to fetch the data from unpkg.com because we are not using the cache.
         const { data, request } = await axios.get(args.path);
 
-        const fileType = args.path.match(/.css$/) ? "css" : "jsx";
-
         // this will be only used for css files. We need to return the contents of the css file.
         const escapedData = data
           .replace(/\n/g, "")
           .replace(/"/g, '\\"')
           .replace(/'/g, "\\'");
 
-        const contents =
-          fileType === "css"
-            ? `
-          const style = document.createElement('style');
-          style.innerText = '${escapedData}';
-          document.head.appendChild(style);
-          `
-            : data;
+        const contents = `
+            const style = document.createElement('style');
+            style.innerText = '${escapedData}';
+            document.head.appendChild(style);
+            `;
+
         const result: esbuild.OnLoadResult = {
           loader: "jsx",
           contents,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
+
+        // Cache the result
+        await fileCache.setItem(args.path, result);
+
+        // return the result
+        return result;
+      });
+
+      build.onLoad({ filter: /^index\.js$/ }, async (args: any) => {
+        return {
+          loader: "jsx",
+          contents: userCode,
+        };
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        // Check if the data is in the cache
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(
+          args.path
+        );
+
+        if (cachedResult) {
+          return cachedResult;
+        }
+
+        // Fetch the data from unpkg.com. We need to fetch the data from unpkg.com because we are not using the cache.
+        const { data, request } = await axios.get(args.path);
+
+        const result: esbuild.OnLoadResult = {
+          loader: "jsx",
+          contents: data,
           resolveDir: new URL("./", request.responseURL).pathname,
         };
 
