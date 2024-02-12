cd8411b8c203e663962da18712da950555c1189d
23navi <navisureka23@gmail.com>
2024-02-12 23:47:03 +0530

    resize window

--- in-browser-transpiling/src/components/resizable.tsx
+++ in-browser-transpiling/src/components/resizable.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import "./resizable.css";
 import { ResizableBox, ResizableBoxProps } from "react-resizable";
 
@@ -7,24 +8,39 @@ interface ResizableProps {
 }
 
 const Resizable = ({ direction, children }: ResizableProps) => {
-  let resizableProps: ResizableBoxProps;
+  const [innerHeight, setInnerHeight] = useState(window.innerHeight);
+  const [innerWidth, setInnerWidth] = useState(window.innerWidth);
 
+  useEffect(() => {
+    const resize = () => {
+      setInnerHeight(window.innerHeight);
+      setInnerWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", resize);
+
+    // Clean up function, which runs when the component is de-mount
+    return () => {
+      window.removeEventListener("resize", resize);
+    };
+  }, []);
+
+  let resizableProps: ResizableBoxProps;
   if (direction === "vertical") {
     resizableProps = {
       // Note: width="100%" doesn't work for ResizableBox so we are using Infinity
       width: Infinity, // default starting height. Will change when we drag it
       height: 300,
-      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      maxConstraints: [Infinity, innerHeight * 0.9],
       minConstraints: [Infinity, 50], // 50px
       resizeHandles: ["s"],
     };
   } else {
     resizableProps = {
       className: "resize-horizontal",
-      width: window.innerWidth * 0.75,
+      width: innerWidth * 0.75,
       height: Infinity,
-      minConstraints: [window.innerHeight * 0.2, Infinity],
-      // maxConstraints: [window.innerHeight * 0.75, Infinity],
+      minConstraints: [innerHeight * 0.2, Infinity],
+      // maxConstraints: [innerHeight * 0.75, Infinity],
       resizeHandles: ["e"],
     };
   }
