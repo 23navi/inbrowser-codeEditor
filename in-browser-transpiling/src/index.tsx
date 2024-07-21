cd4eaac5fe10cce36710084f59559935035f17d4
23navi <navisureka23@gmail.com>
2024-07-21 23:51:00 +0530

    md editor component

--- in-browser-transpiling/src/index.tsx
+++ in-browser-transpiling/src/index.tsx
@@ -1,6 +1,7 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import ReactDOM from "react-dom/client";
-import CodeCell from "./components/code-cell";
+// import CodeCell from "./components/code-cell";
+import TextEditor from "./components/text-editor";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -9,7 +10,8 @@ const root = ReactDOM.createRoot(
 const App = () => {
   return (
     <div>
-      <CodeCell initialCode="const a=10;"></CodeCell>
+      <TextEditor />
+      {/* <CodeCell initialCode="const a=10;"></CodeCell> */}
     </div>
   );
 };
